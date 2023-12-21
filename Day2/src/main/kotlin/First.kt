7939802c31139e47024e4d06d202cfe92228fedb
maslen4 <maslen4@uniba.sk>
2023-12-21 20:16:23 +0100

    Day2

---
+++ Day2/src/main/kotlin/First.kt
@@ -0,0 +1,63 @@
+import java.io.File
+
+
+fun readFile1(fileName: String, map: Map<String, Int>): Int {
+    var result = 0
+    File(fileName).forEachLine {
+        val line = splitLineIntoGameNumberAndBalls(it);
+        val gameNumber = line[0].split(" ")[1]
+        val balls = line[1]
+        val rounds = splitBallsIntoRounds(balls)
+        var isValid = true
+        rounds.forEach { round ->
+            splitRoundIntoColors(round).forEach { color ->
+                val c = splitColorIntoNumberAndColor(stripFromFirstSpace(color));
+                if (map.get(c[1])!! < c[0].toInt()) {
+                    isValid = false
+                }
+            }
+        }
+        result = if (isValid) result + gameNumber.toInt() else result
+    }
+    return result
+}
+
+fun readFile2(fileName: String): Int {
+    var result = 0
+    File(fileName).forEachLine {
+        val line = splitLineIntoGameNumberAndBalls(it);
+        val balls = line[1]
+        val rounds = splitBallsIntoRounds(balls)
+        val map = mutableMapOf("red" to 0, "blue" to 0, "green" to 0)
+        rounds.forEach { round ->
+            splitRoundIntoColors(round).forEach { color ->
+                val c = splitColorIntoNumberAndColor(stripFromFirstSpace(color));
+                if (map.get(c[1])!! < c[0].toInt()) {
+                    map.set(c[1], c[0].toInt())
+                }
+            }
+        }
+        var power = 1
+        for (value in map.values) {power *= value}
+        result += power
+    }
+    return result
+}
+
+fun splitLineIntoGameNumberAndBalls(line: String): List<String> = line.split(":")
+
+fun splitBallsIntoRounds(line: String): List<String> = line.split(";")
+
+fun splitRoundIntoColors(line: String): List<String> = line.split(",")
+
+fun splitColorIntoNumberAndColor(line: String): List<String> = line.split(" ")
+
+fun stripFromFirstSpace(line: String): String = line.removePrefix(" ")
+
+
+
+fun main(args: Array<String>) {
+    val map: Map<String, Int> = mutableMapOf("red" to 12, "blue" to 14, "green" to 13)
+    println(readFile1("input.txt", map))
+    println(readFile2("input.txt"))
+}
